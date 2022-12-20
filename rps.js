17f671e075dac95f96d67c4eee4b99861c1eff14
ShalaG <104413693+itsshalag@users.noreply.github.com>
2022-12-20 18:58:01 -0500

    feat add function rounds that go up to five Add a function that makes the R.P.S go five times then decide winner.

--- rps.js
+++ rps.js
@@ -1,38 +1,49 @@
-const choices=["rock", "paper", "scissors"];
+const choices = ["rock", "paper", "scissors"];
 
-
-//computer Random choice
-let getComputerChoice=()=>{
-   
-    let options = choices[Math.floor(Math.random()*choices.length)];
-    return options;
-    
+let getComputerChoice =() =>{
+    computerSelection = choices[ Math.floor((Math.random() *choices.length))]; 
+    console.log( "here is computer's choice: " + computerSelection);
+    return computerSelection; 
 }
 
-//A round of R.P.S. that compares the both choices 
-//if player and compu play same weapon then its a tie and return tie and end it ther
-//or if player plays paper and compu plays rock
-//or if player plays scissors and compu plays paper player wins
-//or if player plays rock and compu plays scissor player wins
-//or if they play the same thing its a tie
-let playRound=(playerSelection, computerSelection)=>{
 
-    if(playerSelection == computerSelection){
-        return "its a tie";
-    } else if (playerSelection == "paper" && computerSelection == "rock" ||
-        playerSelection ==  "scissors" && computerSelection == "paper" ||
-        playerSelection == "rock" && computerSelection == "scissors")
-        {
-            return "user beat compu"; 
+const getPlayerChoice = () => {
+    playerSelection= lowerCaseChoice();
+    choiceIncludes= choices.includes(playerSelection);
+        while(choiceIncludes === true){
+            console.log("here is player Choice: " + playerSelection);
+            return playerSelection;
+        }
+         if(choiceIncludes === false)
+            getPlayerChoice();        
+       
+}
+
+const lowerCaseChoice = () => {
+    playerChoice=prompt("Enter: rock, paper or scissors");
+    if (playerChoice === null || playerChoice === ''){
+        alert("You did not enter a choice");
+        lowerCaseChoice(); 
     } else {
-            return "computer beat player";
-    }
-    
-} 
-const playerSelection = "rock";
-const computerSelection =getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+        playerChoice= playerChoice.toLowerCase();
+        return playerChoice;  
+      }
+}
 
-let game=()=>{
-    
+let playRound = (playerSelection, computerSelection) => {
+    playerSelection=getPlayerChoice();
+    computerSelection=getComputerChoice();
+
+    if (playerSelection === computerSelection){
+        console.log("it's a tie");
+    } else if ( playerSelection === "rock" && computerSelection === "scissors" ||
+                playerSelection === "scissor" && computerSelection === paper ||
+                playerSelection === "paper" && computerSelection === "scissors"){
+                    console.log("player beat computer");
+    } else console.log("computer wins");
+}
+playRound(); 
+
+const chooseWinner =() => {
+
 }
